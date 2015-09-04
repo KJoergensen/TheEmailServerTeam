a166e3a077db9fec5a5978f5c4d57c80534168b1
HisayoNakayama <hisaviola@gmail.com>
2015-09-04 20:58:57 +0200

    Added LoginView

--- project/src/main/java/Views/LoginView.java
+++ project/src/main/java/Views/LoginView.java
@@ -3,9 +3,8 @@ package Views;
 import javax.swing.*;
 import java.awt.*;
 
-import static javax.swing.WindowConstants.*;
 
-/**
+/***
  * Created by Hisayo on 04/09/15.
  */
 public class LoginView extends JFrame{
@@ -13,9 +12,7 @@ public class LoginView extends JFrame{
     static final String LOGIN ="Login";
     static final String CANCEL = "Cancel";
 
-    private JPanel mainPanel, southPanel, centerPanel, northPanel;
     private JButton cancelBtn, loginBtn;
-    private JLabel emailLabel, passwordLabel;
     private JTextField emailTxtF, passwordTxtF;
 
     public LoginView()
@@ -37,25 +34,25 @@ public class LoginView extends JFrame{
 
     private JPanel createUIComponents() {
 
-        this.northPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        this.emailLabel = new JLabel("Gmail address");
+        JPanel northPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        JLabel emailLabel = new JLabel("Gmail address");
         this.emailTxtF = new JTextField(20);
-        this.northPanel.add(emailLabel);
-        this.northPanel.add(emailTxtF);
+        northPanel.add(emailLabel);
+        northPanel.add(emailTxtF);
 
-        this.centerPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        this.passwordLabel = new JLabel("Password");
+        JPanel centerPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        JLabel passwordLabel = new JLabel("Password");
         this.passwordTxtF = new JTextField(20);
-        this.centerPanel.add(passwordLabel);
-        this.centerPanel.add(passwordTxtF);
+        centerPanel.add(passwordLabel);
+        centerPanel.add(passwordTxtF);
 
-        this.southPanel = new JPanel(new FlowLayout());
+        JPanel southPanel = new JPanel(new FlowLayout());
         this.loginBtn = new JButton(LOGIN);
         this.cancelBtn = new JButton(CANCEL);
-        this.southPanel.add(loginBtn);
-        this.southPanel.add(cancelBtn);
+        southPanel.add(loginBtn);
+        southPanel.add(cancelBtn);
 
-        this.mainPanel = new JPanel(new GridLayout(3,1));
+        JPanel mainPanel = new JPanel(new GridLayout(3, 1));
         mainPanel.add(northPanel);
         mainPanel.add(centerPanel);
         mainPanel.add(southPanel);
