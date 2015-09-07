1c8b8ffb96b963c69a6daeae494d855543923608
Anders Nadhazi <ahnadhazi@gmail.com>
2015-09-07 15:23:23 +0200

    cleaned code in email receiver

--- project/src/main/java/Models/User.java
+++ project/src/main/java/Models/User.java
@@ -3,7 +3,7 @@ package Models;
 import java.util.ArrayList;
 
 public class User {
-    public String userName;
+    public String username;
     public String password;
     public int id;
     public ArrayList<Email> inbox;
--- project/src/main/java/Utilities/EmailReceiver.java
+++ project/src/main/java/Utilities/EmailReceiver.java
@@ -1,15 +1,25 @@
 package Utilities;
 
 import Models.Email;
-import com.sun.mail.util.MailSSLSocketFactory;
 
-import javax.mail.*;
-import javax.mail.search.FlagTerm;
-import java.security.GeneralSecurityException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Properties;
+import javax.mail.Address;
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.Message.RecipientType;
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
+import javax.mail.Session;
+import javax.mail.Store;
+
 
 public class EmailReceiver
 {
+    String protocol = "imaps";
+    String host = "google";
+    String port = "1337";
+
     public ArrayList<Email> getEmails ()
     {
         ArrayList<Email> list = new ArrayList<Email>();
@@ -19,92 +29,62 @@ public class EmailReceiver
         return list;
     }
 
-    public void downloadEmails(String protocol, String host, String port,
-                               String userName, String password, long tennantId) throws Exception {
-        try {
+    public void downloadEmails(String userName, String password, int id, ArrayList<Email> list) throws Exception {
+        Properties properties = getServerProperties(protocol, host, port);
+        Session session = Session.getDefaultInstance(properties);
 
-            Properties properties = getPropertiesForProtocol(protocol, host,
-                    port);
-            Session session = Session.getInstance(properties,
-                    new javax.mail.Authenticator() {
-                        protected PasswordAuthentication getPasswordAuthentication() {
-                            return new PasswordAuthentication(userName,
-                                    password);// Specify the Username and the
-                            // PassWord
-                        }
-                    });
+        try {
+            // connects to the message store
             Store store = session.getStore(protocol);
             store.connect(userName, password);
 
             // opens the inbox folder
             Folder folderInbox = store.getFolder("INBOX");
-            folderInbox.open(Folder.READ_WRITE);
-            System.out.println("HOST: " + store.getURLName().getHost());
-            // fetches new messages from server
+            folderInbox.open(Folder.READ_ONLY);
 
-            Message[] messages = folderInbox.search(new FlagTerm(new Flags(
-                    Flags.Flag.SEEN), false));
+            // fetches new messages from server
+            Message[] messages = folderInbox.getMessages();
+
             for (int i = 0; i < messages.length; i++) {
                 Message msg = messages[i];
-                String attachments = "";
-//				Collection<byte[]> attachments = new ArrayList<byte[]>(getAttachmentFiles(msg));
-                System.out.println(String.format("Is recent: %s, Is seen: %s",
-                        msg.isSet(Flags.Flag.RECENT),
-                        msg.isSet(Flags.Flag.SEEN)));
                 Address[] fromAddress = msg.getFrom();
                 String from = fromAddress[0].toString();
                 String subject = msg.getSubject();
                 String toList = parseAddresses(msg
                         .getRecipients(Message.RecipientType.TO));
-                // Creates arraylists to contain Collection elements for TO.
-                Collection<String> JMATolist = new ArrayList<String>();
-                JMATolist.add(toList);
                 String ccList = parseAddresses(msg
-                        .getRecipients(Message.RecipientType.CC));
-                // Creates arraylists to contain Collection elements for CC.
-                Collection<String> JMACcList = new ArrayList<String>();
-                JMACcList.add(ccList);
-                // Creates arraylists to contain Collection elements for
-                // BCC.
-                String bccList = parseAddresses(msg
-                        .getRecipients(Message.RecipientType.BCC));
-                Collection<String> JMABccList = new ArrayList<String>();
-                JMABccList.add(bccList);
-                Date sentDate = msg.getSentDate();
-                Address[] replyTo = msg.getReplyTo();
-                // Casts replyTo to a String.
-                String JMAReplyTo = replyTo.toString();
-                int messageId = i + 1;
+                        .getRecipients(RecipientType.CC));
+                String sentDate = msg.getSentDate().toString();
 
                 String contentType = msg.getContentType();
                 String messageContent = "";
 
-                // Creates email object with all received email's.
-                RFC822Message Message = RFC822Message.newBuilder()
-                        .messageId(i + 1 + "")
-                        .date(sentDate)
-                        .tenantId(tennantId)
-                        .from(from)
-                        .to(JMATolist)
-                        .replyTo(JMAReplyTo)
-                        .cc(JMABccList)
-                        .bcc(JMABccList)
-                        .subject(subject)
-                        .body(messageContent)
-                                //.attachments(attachments)
-                        .build();
-//				RFC822Message Message = new RFC822Message(i + 1 + "", tennantId,
-//						sentDate, from, subject, JMAReplyTo, JMATolist,
-//						JMACcList, JMABccList, null, null, messageContent,
-//						null, attachments);
-                //Message.setAttachment();
-                m_messageQueue.write(Message);
-                msg.setFlag(Flags.Flag.SEEN, true);
+                if (contentType.contains("text/plain")
+                        || contentType.contains("text/html")) {
+                    try {
+                        Object content = msg.getContent();
+                        if (content != null) {
+                            messageContent = content.toString();
+                        }
+                    } catch (Exception ex) {
+                        messageContent = "[Error downloading content]";
+                        ex.printStackTrace();
+                    }
+                }
+
+                // print out details of each message
+                System.out.println("Message #" + (i + 1) + ":");
+                System.out.println("\t From: " + from);
+                System.out.println("\t To: " + toList);
+                System.out.println("\t CC: " + ccList);
+                System.out.println("\t Subject: " + subject);
+                System.out.println("\t Sent Date: " + sentDate);
+                System.out.println("\t Message: " + messageContent);
             }
+
+            // disconnect
             folderInbox.close(false);
             store.close();
-            // disconnect
-
         } catch (NoSuchProviderException ex) {
             System.out.println("No provider for protocol: " + protocol);
             ex.printStackTrace();
@@ -113,29 +93,43 @@ public class EmailReceiver
             ex.printStackTrace();
         }
     }
-    private Properties getPropertiesForProtocol(String protocol, String host,
-                                                String port) throws GeneralSecurityException {
-        Properties properties;
-        if (protocol.equalsIgnoreCase("imaps")
-                || protocol.equalsIgnoreCase("imap")) {
-            properties = getServerProperties(protocol, host, port);
-            properties.put("mail.imaps.ssl.enable", true);
-            properties.put("mail.imaps.ssl.trust", "*");
-            MailSSLSocketFactory factory = new MailSSLSocketFactory();
-            factory.setTrustAllHosts(true);
-            properties.put("mail.imap.ssl.socketFactory", factory);
-        } else if (protocol.equalsIgnoreCase("pop3")) {
-            properties = getServerProperties(protocol, host, port);
-            properties.put("mail.pop3.ssl.enable", true);
-            properties.put("mail.pop3.ssl.trust", "*");
-            MailSSLSocketFactory factory = new MailSSLSocketFactory();
-            factory.setTrustAllHosts(true);
-            properties.put("mail.pop3.ssl.socketFactory", factory);
-        } else {
-            throw new IllegalArgumentException(
-                    "Protocol should be either imap/imaps or pop3");
-        }
+
+    private Properties getServerProperties(String protocol, String host,
+                                           String port) {
+        Properties properties = new Properties();
+
+        // server setting
+        properties.put(String.format("mail.%s.host", protocol), host);
+        properties.put(String.format("mail.%s.port", protocol), port);
+
+        // SSL setting
+        properties.setProperty(
+                String.format("mail.%s.socketFactory.class", protocol),
+                "javax.net.ssl.SSLSocketFactory");
+        properties.setProperty(
+                String.format("mail.%s.socketFactory.fallback", protocol),
+                "false");
+        properties.setProperty(
+                String.format("mail.%s.socketFactory.port", protocol),
+                String.valueOf(port));
+
         return properties;
     }
+
+    private String parseAddresses(Address[] address) {
+        String listAddress = "";
+
+        if (address != null) {
+            for (int i = 0; i < address.length; i++) {
+                listAddress += address[i].toString() + ", ";
+            }
+        }
+        if (listAddress.length() > 1) {
+            listAddress = listAddress.substring(0, listAddress.length() - 2);
+        }
+
+        return listAddress;
+    }
+
 
 }
