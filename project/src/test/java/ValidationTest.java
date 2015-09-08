aa3d09638244880dc1b005e068c686e50043ad41
THyldgaard <t.hyldgaard92@gmail.com>
2015-09-08 15:24:19 +0200

    Made some unit test for validations

---
+++ project/src/test/java/ValidationTest.java
@@ -0,0 +1,34 @@
+import Services.Validator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Hyldgaard on 9/8/15.
+ */
+public class ValidationTest {
+
+    @Test
+    public void validateCorrectEmailEndingTrueTest() {
+        String email = "test@gmail.com";
+        Validator validator = new Validator();
+        assertTrue(validator.validateCorrectEmailEnding(email));
+    }
+
+    @Test
+    public void validateCorrectEmailEndingFalseTest() {
+        String email = "test@hotmail.com";
+        Validator validator = new Validator();
+        assertFalse(validator.validateCorrectEmailEnding(email));
+    }
+
+    @Test
+    public void validateCorrectEmailEndingFalseNonEmailTest() {
+        String email = "tetsteafsfae.com";
+        Validator validator = new Validator();
+        assertFalse(validator.validateCorrectEmailEnding(email));
+
+    }
+
+}
