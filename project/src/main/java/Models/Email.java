391d203a46ea72eeabc34be35e73a837c2d2c122
THyldgaard <t.hyldgaard92@gmail.com>
2015-09-08 09:01:14 +0200

    Added getters, setters and contructors for email

--- project/src/main/java/Models/Email.java
+++ project/src/main/java/Models/Email.java
@@ -10,4 +10,70 @@ public class Email {
     public int id;
     public Date date;
     public boolean newEmail; // To check if read/unread
+
+    public Email() {}
+
+    public Email(String to, String from, String subject, String body, int id, Date date, boolean newEmail) {
+        this.to = to;
+        this.from = from;
+        this.subject = subject;
+        this.body = body;
+        this.id = id;
+        this.date = date;
+        this.newEmail = newEmail;
+    }
+
+    public String getTo() { return to; }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public boolean isNewEmail() {
+        return newEmail;
+    }
+
+    public void setNewEmail(boolean newEmail) {
+        this.newEmail = newEmail;
+    }
 }
