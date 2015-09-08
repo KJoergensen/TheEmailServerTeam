fce38fd8aa57a977647e8aa450d23f814b7867ad
THyldgaard <t.hyldgaard92@gmail.com>
2015-09-08 09:01:45 +0200

    Added getters, setters and contructors for user.

--- project/src/main/java/Models/User.java
+++ project/src/main/java/Models/User.java
@@ -7,4 +7,48 @@ public class User {
     public String password;
     public int id;
     public ArrayList<Email> inbox;
+
+    public User() {}
+
+    public User(String username, String password, int id, ArrayList<Email> inbox) {
+        this.username = username;
+        this.password = password;
+        this.id = id;
+        this.inbox = inbox;
+    }
+
+    // getters and setters
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ArrayList<Email> getInbox() {
+        return inbox;
+    }
+
+    public void setInbox(ArrayList<Email> inbox) {
+        this.inbox = inbox;
+    }
+
 }
