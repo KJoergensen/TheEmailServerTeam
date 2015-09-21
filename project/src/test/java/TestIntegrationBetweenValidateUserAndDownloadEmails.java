37c27b70d834d44cd64a360429b6d0e83a15842b
THyldgaard <t.hyldgaard92@gmail.com>
2015-09-21 14:03:24 +0200

    Created an itegration test between user and download email

--- project/src/test/java/TestIntegrationBetweenValidateUserAndDownloadEmails.java
+++ project/src/test/java/TestIntegrationBetweenValidateUserAndDownloadEmails.java
@@ -1,5 +1,35 @@
+import Models.User;
+import Utilities.EmailReceiver;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 /**
  * Created by Hyldgaard on 9/21/15.
  */
 public class TestIntegrationBetweenValidateUserAndDownloadEmails {
+
+    @Test
+    public void downloadEmailTrueTest() {
+        User user = new User();
+        user.setUsername("keamailtest@gmail.com");
+        user.setPassword("mailtester");
+
+        EmailReceiver receiver = new EmailReceiver();
+
+        org.junit.Assert.assertTrue(!receiver.downloadEmails(user.getUsername(), user.getPassword()).isEmpty());
+
+    }
+
+    @Test
+    public void downloadEmailFalseTest() {
+        User user = new User();
+        user.setUsername("keamailtest@gmail.com");
+        user.setPassword("");
+
+        EmailReceiver receiver = new EmailReceiver();
+
+        org.junit.Assert.assertTrue(receiver.downloadEmails(user.getUsername(), user.getPassword()).isEmpty());
+    }
+
 }
